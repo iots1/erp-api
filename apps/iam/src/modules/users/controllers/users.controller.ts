8cdcab6859621ae9b7aade776070c9d6cad5deec
PEEPO <iotechsoft@gmail.com>
2026-07-22 00:43:27 +0700

    feat: add Public decorator to test graceful shutdown endpoint

--- apps/iam/src/modules/users/controllers/users.controller.ts
+++ apps/iam/src/modules/users/controllers/users.controller.ts
@@ -14,6 +14,7 @@ import { ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import {
   CurrentUser,
   IResponsePaginatedService,
+  Public,
   RequirePermission,
   type IUserSession,
   ParseUuidParamPipe,
@@ -59,6 +60,7 @@ export class UsersController extends BaseControllerOperations<
   }
 
   @Get('test-graceful-shutdown')
+  @Public()
   @ApiOperation({
     summary: 'Test Graceful Shutdown by simulating a long-running process',
   })
